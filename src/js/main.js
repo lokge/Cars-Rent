6dcf6e1de48c1a1a2755371ce9d674b62e116189
Erkin <dzhamanbaev04@gmail.com>
2025-06-10 16:15:03 +0600

    main pages done

---
+++ src/js/main.js
@@ -0,0 +1,33 @@
+function onEntry(entries) {
+    entries.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('visible');
+        }
+    });
+}
+
+let options = {
+    threshold: [0.5]
+};
+let observer = new IntersectionObserver(onEntry, options);
+let descriptionCards = document.querySelectorAll('.description-card');
+
+for (let card of descriptionCards) {
+    observer.observe(card);
+}
+
+let descriptionCarLogos = document.querySelector('.description-car-logos');
+observer.observe(descriptionCarLogos);
+
+let choseUsCards = document.querySelectorAll('.chose-us-card');
+
+for (let card of choseUsCards) {
+    observer.observe(card);
+}
+
+let choseUsContainers = document.querySelectorAll('.chose-us .container');
+
+console.log(choseUsContainers)
+for (let card of choseUsContainers) {
+    observer.observe(card);
+}
